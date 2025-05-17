a3765d3007973edbebe8049bbf85d8f4f364101b
EslamKamel89 <elsamkamelforex@gmail.com>
2025-05-17 23:48:29 +0300

    refactor: update link in AppHeader to point to home route and comment out dashboard route

--- routes/web.php
+++ routes/web.php
@@ -9,9 +9,9 @@ use Inertia\Inertia;
 Route::get('/', function () {
     return Inertia::render('home/Index');
 })->name('home');
-Route::get('dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('dashboard', function () {
+//     return Inertia::render('Dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__ . '/settings.php';
 require __DIR__ . '/auth.php';
