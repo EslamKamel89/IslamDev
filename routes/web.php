5d2d4fa3671d96a3a965358f409820bb0cf7a852
EslamKamel89 <elsamkamelforex@gmail.com>
2026-02-18 17:25:54 +0200

    chore: update gitignore, comment test route, and sync ide helper docblock

--- routes/web.php
+++ routes/web.php
@@ -23,8 +23,8 @@ Route::get('/resume', ResumeController::class)->name('resume');
 // Route::get('dashboard', function () {
 //     return Inertia::render('Dashboard');
 // })->middleware(['auth', 'verified'])->name('dashboard');
-Route::get('/test', function () {
-    echo (new MessageNotification('Eslam kamel', 'admin@example.com', 'test message', now()))->render();
-});
+// Route::get('/test', function () {
+//     echo (new MessageNotification('Eslam kamel', 'admin@example.com', 'test message', now()))->render();
+// });
 require __DIR__ . '/settings.php';
 require __DIR__ . '/auth.php';
